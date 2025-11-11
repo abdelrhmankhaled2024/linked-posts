d6306bf0e9a29969b332f38828a1bab9c5b6097f
abdelrhmankhaled2024 <152085187+abdelrhmankhaled2024@users.noreply.github.com>
2025-11-11 17:00:09 +0200

    done

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,15 +1,24 @@
 import mysql from "mysql2";
 
 export const dbConnection = () => {
-  const conn = mysql.createConnection(
-    "mysql://ut2oiu3yu2vsy7eh:PRKml8JjbG840IRT3jqG@bxq6mpfnn1eleztgfrtt-mysql.services.clever-cloud.com:3306/bxq6mpfnn1eleztgfrtt"
-  );
+  const conn = mysql.createConnection({
+    host: "bxq6mpfnn1eleztgfrtt-mysql.services.clever-cloud.com",
+    user: "ut2oiu3yu2vsy7eh",
+    password: "PRKml8JjbG840IRT3jqG",
+    database: "bxq6mpfnn1eleztgfrtt",
+    port: 3306,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
   conn.connect((err) => {
     if (err) {
       console.log("database error:", err);
     } else {
-      console.log("database Connected Succsefully.");
+      console.log("Database Connected Successfully.");
     }
   });
+
   return conn;
 };
